8cb6989fdc8479f733b95b95d8988dc5f6343055
tesdanez <50134789+tesdanez@users.noreply.github.com>
2020-05-29 21:52:27 +0800

    Add files via upload

---
+++ pracBlog.js
@@ -0,0 +1,19 @@
+
+
+function toggle() {
+    let toggleActive = document.querySelector('.menu');
+    let toggle = document.querySelector('.toggle');
+
+    toggleActive.classList.toggle('activeMenu');
+
+    toggle.classList.toggle('toggleLine');
+}
+
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 550);
+});
+
+
+
+
+
